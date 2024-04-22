1ffb1f7bc7b4673b395dec39d6b40c20888e5f59
Beata Bolechowicz <rubinxv01@gmail.com>
2024-04-22 19:59:39 +0200

    lack of decoder

--- lab7.8/main.py
+++ lab7.8/main.py
@@ -13,7 +13,7 @@ def prob(channel):
     S = np.mean(channel)
 
     if S >= 2:
-        p = (S-1)/2
+        p = (S-1)/S
     else:
         p = 0.5
 
@@ -22,18 +22,18 @@ def prob(channel):
 def rice_enc(N, k):
     v = []
     u = []
-    u = [f_u(n, k) for n in N]
+    u = [int(round(f_u(n, k))) for n in N]
+
 
     if k != 0:
-        v = [f_v(ni, k, ui) for ni, ui in zip(N, u)]
+        v = [int(round(f_v(ni, k, ui))) for ni, ui in zip(N, u)]
         cnt = 0
 
-        print(v)
 
         for i in range(len(v)):
             vn = ''
             while(v[i] != 0 or cnt != k):
-                vn = str(int(v[i]) & 1) + vn
+                vn = str(v[i] & 1) + vn
                 v[i] >>= 1
                 cnt += 1  
             
@@ -47,9 +47,10 @@ def rice_enc(N, k):
         u[i] = '0' * (u[i])
         u[i] += '1'
 
-    out = []
+    out = ''
     for ui, vi in zip(u, v):
-        out.append(ui + vi)
+        out += (ui + vi)
+        # out.append(ui + vi)
         # out.append(ui + ':' + vi)
         # print(out[-1])
 
@@ -68,64 +69,38 @@ def differ_code(channel):
     return result
 
 
-def count_entropy(matrix, e_min, e_max, size):
-    occur = Counter(matrix)
-    p = np.zeros(e_max, dtype=np.float64)
-    
-    if e_min < 0:
-        p1 = np.zeros(-e_min, dtype=np.float64)
-    
-    entropy = 0
-
-    #prawdopodobienstwo
-    for key in occur.keys():
-        if key < 0:
-            p1[int(key)] += (abs(occur[key]) / (size))
-        else:
-            p[int(key)] += (abs(occur[key]) / (size))
-
-    if e_min < 0:
-        p = np.concatenate((p1, p))
-        # print(p.size)
-
-    for i in range(e_min, e_max):   
-        if(p[i] == 0):  #log2(0) = -inf
-            continue
-
-        entropy += p[i]*np.log2(p[i])  
-
-    return -entropy
-
-
-dir = "C:/Users/rubin/OneDrive/Pulpit/TIIK/lab7.8/audio"
+dir = "C:/Users/bolec/OneDrive/Pulpit/TIIK/lab7.8/audio"
 audio_files = [f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir, f))]
 for file in audio_files:
     audio_data = librosa.load(os.path.join(dir, file), sr=None, mono=False)
-    # print(audio_data)
     left_channel = audio_data[0][0]
     right_channel = audio_data[0][1]
     left_channel = np.floor(left_channel * float(2**15) + 0.5)
-    right_channel = np.floor(right_channel * float(2**15) + 0.5)  
+    right_channel = np.floor(right_channel * float(2**15) + 0.5) 
 
     #differ
     left_encoded = differ_code(left_channel)
     right_encoded = differ_code(right_channel)
 
+    print(left_encoded)
+    print(right_encoded)
+
     for i in range(len(left_encoded)):
-        if left_encoded[i] > 0:
-            left_encoded[i] = left_encoded[i] * 2
+        if left_encoded[i] >= 0:
+            left_encoded[i] = 2 * left_encoded[i]
         else:
             left_encoded[i] = (-2 * left_encoded[i]) - 1
 
     p_l = prob(left_encoded)
     k_l = f_k(p_l)
-    out_r = rice_enc(left_encoded, k_l)
 
     if k_l > 15:
         k_l = 15
 
+    out_l = rice_enc(left_encoded, k_l)
+
     for i in range(len(right_encoded)):
-        if right_encoded[i] > 0:
+        if right_encoded[i] >= 0:
             right_encoded[i] = right_encoded[i] * 2
         else:
             right_encoded[i] = (-2 * right_encoded[i]) - 1
@@ -135,3 +110,13 @@ for file in audio_files:
 
     if k_r > 15:
         k_r = 15
+
+    out_r = rice_enc(right_encoded, k_r)
+
+    # print(out_l)
+    print(len(out_l))
+    print(len(out_r))
+    l = len(out_l) + len(out_r)
+    print('coded len: ', l)
+    LR = l/(len(right_encoded)+len(left_encoded))
+    print('LR: ', LR)
